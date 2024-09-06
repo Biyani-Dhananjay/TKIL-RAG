fa27520ef0227a459f90814abfd712fe837ff141
Biyani-Dhananjay <dhananjay.biyani@ellicium.com>
2024-09-06 11:18:30 +0530

    Update :  app.py

--- app.py
+++ app.py
@@ -17,8 +17,7 @@ uploaded_file  = st.sidebar.file_uploader("Upload a file", type=["pdf"] , key="f
 if uploaded_file:
     file_name = uploaded_file.name
     pdf_folder_path = os.path.join(path_to_store_data , file_name.split('.')[0])
-    
-    # os.makedirs(pdf_folder_path, exist_ok=True)
+
     if not os.path.exists(pdf_folder_path):
         os.makedirs(pdf_folder_path)
     
@@ -29,6 +28,13 @@ if uploaded_file:
     
     pdf_reader = PdfReader(file_path)
     number_of_pages = len(pdf_reader.pages)
-    for i in range(number_of_pages):
-        page = pdf_reader.pages(i)
-        st.write(page.extractText())
+    for page_num in range(number_of_pages):
+        page = pdf_reader.pages[page_num]
+        page_text = page.extract_text()
+        st.write(page_text)
+        
+names_of_folders = os.listdir(path_to_store_data)
+selected_pdf_folder = st.sidebar.selectbox("Already Uploaded PDF's", names_of_folders,index=None, placeholder="Please select a pdf")
+
+if selected_pdf_folder:
+    st.write("PDF selected is ", selected_pdf_folder)
