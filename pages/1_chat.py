cd8501f0bf1387bab2f7ce1f02e054b5530ea56c
Dhananjay Biyani <dhananjay.biyani@ellicium.com>
2025-09-11 15:09:22 +0530

    fix: dimensions of logo

--- pages/1_chat.py
+++ pages/1_chat.py
@@ -167,8 +167,8 @@ graph = load_graph_pkl(os.path.join(vectorstore_folder, f"{selected_option}{file
 st.html("""
   <style>
     [alt=Logo] {
-      height: 45px;
-        width: 8rem;
+      height: 100px;
+        width: 20rem;
     }
   </style>
         """)
